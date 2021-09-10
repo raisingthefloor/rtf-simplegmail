9e447ee954f478aff1123e0124eab3f23f58d195
Hardik Shah <hardik@raisingthefloor.org>
2021-09-10 16:49:33 +0530

    - disable hmr

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,19 @@
- import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 4000,
-    proxy: {
-      '/api':{
-        target: process.env.TARGET_URL,
-        changeOrigin: true,
-        secure: true
-      }
+    plugins: [vue()],
+    server: {
+        hmr: false,
+        host: '0.0.0.0',
+        port: process.env.PORT || 4000,
+        proxy: {
+            '/api': {
+                target: process.env.TARGET_URL,
+                changeOrigin: true,
+                secure: true
+            }
+        }
     }
-  }
 })
