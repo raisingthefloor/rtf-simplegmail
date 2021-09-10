8f1f5dcd58cadaf808b0da41b291868ea9da77b8
Hardik Shah <hardik@raisingthefloor.org>
2021-09-10 18:02:16 +0530

    - Added SSL config inside vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,14 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import * as fs from "fs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
     server: {
-        hmr: {
-            protocol: 'ws'
+        https: {
+            key: fs.readFileSync(`/etc/letsencrypt/live/${process.env.SSL_DOMAIN}/privkey.pem`),
+            cert: fs.readFileSync(`/etc/letsencrypt/live/${process.env.SSL_DOMAIN}/cert.pem`),
         },
         host: '0.0.0.0',
         port: process.env.PORT || 4000,
