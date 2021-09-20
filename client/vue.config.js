51de704725c66327cd1d4678a0f035b15b76e0cb
Hardik Shah <hardik@raisingthefloor.org>
2021-09-20 15:46:57 +0530

    - disable host check for devserver

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,11 +1,12 @@
 module.exports = {
     devServer: {
-      proxy: {
-        '^/api': {
-          target: process.env.TARGET_URL,
-          changeOrigin: true,
-          secure: true
-        },
-      }
+        disableHostCheck: true,
+        proxy: {
+            '^/api': {
+                target: process.env.TARGET_URL,
+                changeOrigin: true,
+                secure: true
+            },
+        }
     }
-  }
+}
